ed2290e9b2ce166f79e3bf2b3bd6bdf15a5af90c
Vinit Desai <vdesai5@ncsu.edu>
2023-04-28 02:43:06 -0400

    change comment

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,12 +4,13 @@ cp ./config/secrets.yml.example ./config/secrets.yml
 # Remove default development configuration
 sed '7,9d' ./config/database.yml.example  > ./config/database.yml
 echo "development:
-adapter: mysql2
-host: mysql
-username: root
-password: expertiza
-database: expertiza_production" >> ./config/database.yml
-# Begin to mount
+\tadapter: mysql2
+\thost: mysql
+\tusername: root
+\tpassword: expertiza
+\tdatabase: expertiza_production" >> ./config/database.yml
+# Populate database
 echo "Inserting scrubbed db into database... this may take awhile!"
 mysql --user=root --password=expertiza --host=mysql expertiza_production < ./expertiza.sql
+# Run initializing commands
 bundle exec rake db:migrate RAILS_ENV=development
