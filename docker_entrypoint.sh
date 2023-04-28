bcdab11130ace024067a313cc3138619d8b12a85
Vinit Desai <vdesai5@ncsu.edu>
2023-04-28 04:30:06 -0400

    initialization script

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/bash                                                                                                                          
-echo "Initializing container"
+echo "Running bundle install..."
+bundle install
+echo "Configuring yaml files..."
 cp ./config/secrets.yml.example ./config/secrets.yml
 # Remove default development configuration
 sed '7,9d' ./config/database.yml.example  > ./config/database.yml
-echo "development:
-\tadapter: mysql2
-\thost: mysql
-\tusername: root
-\tpassword: expertiza
-\tdatabase: expertiza_production" >> ./config/database.yml
+echo "
+development:
+  adapter: mysql2
+  host: mysql
+  username: root
+  password: expertiza
+  database: expertiza_production" >> ./config/database.yml
 # Populate database
 echo "Inserting scrubbed db into database... this may take awhile!"
 mysql --user=root --password=expertiza --host=mysql expertiza_production < ./expertiza.sql
 # Run initializing commands
+echo "Running migrations..."
 bundle exec rake db:migrate RAILS_ENV=development
